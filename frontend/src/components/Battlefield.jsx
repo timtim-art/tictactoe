6ff2b912b49ead54dfa8f9e790ea4e08ed8b2065
Tim Groshaupt <timgroshaupt@googlemail.com>
2024-08-27 22:31:07 -0300

    - basic setup of compinents Battlefield and Warrior - first draft to position the warriors in the parent element (Battlefield)

---
+++ frontend/src/components/Battlefield.jsx
@@ -0,0 +1,36 @@
+import Warrior from "./Warrior.jsx";
+
+
+function Battlefield() {
+
+    const warrior_list = [
+        {
+            'id': 1,
+            'type': 'scissor',
+            'x': 20,
+            'y': 300
+        },
+        {
+            'id': 2,
+            'type': 'stone',
+            'x': 200,
+            'y': 120
+        },
+        {
+            'id': 3,
+            'type': 'paper',
+            'x': 350,
+            'y': 10
+        }
+    ]
+
+    return (
+        <div className="w-96 h-96 border-4 border-black relative">
+            <h1 className="text-center">This is the battlefield</h1>
+            {warrior_list.map(warrior => <Warrior key={warrior.id} type={warrior.type} x={warrior.x} y={warrior.y}/>)}
+
+        </div>
+    )
+}
+
+export default Battlefield
